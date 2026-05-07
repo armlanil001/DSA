7f029859d5f1472d0207174aa3da140e9b5d6c20
Aaromal Anil <armlanil001@gmail.com>
2026-05-07 21:47:21 +0530

    SingleNumber.js

---
+++ singleNumber.js
@@ -0,0 +1,16 @@
+var singleNumber = function(nums) {
+    let hash = {};
+
+    for(let i = 0; i < nums.length; i++) {  // loop 1
+        if(!hash[nums[i]]) {
+            hash[nums[i]] = 1;
+        } else {
+            hash[nums[i]]++;
+        }
+    }
+    for(let i = 0; i < nums.length; i++) {  // loop 2
+        if(hash[nums[i]] == 1) {
+            return nums[i];
+        }
+    }
+};

d6f40349fdcbd2357b0460d30a05f1d1ce54a5ac
Aaromal Anil <armlanil001@gmail.com>
2026-05-14 21:43:06 +0530

    important

--- recursion.js
+++ recursion.js
@@ -19,4 +19,40 @@ function printNn(num){
   
   printNn(++num)
 }
-printNn(1);
+// printNn(1);
+
+function sum(n){
+  if(n==0)return 0;
+  return n + sum(n-1);
+}
+// console.log(sum(5));
+
+
+
+
+//sum of nos in an array
+
+let arr = [1,2,3,4,5]
+function sumArray(n){
+  if(n==0) return arr[0];
+  return arr[n]+sumArray(n-1)
+
+}
+
+// console.log(sumArray(arr.length-1));
+
+
+// sum o0f odd numbers
+
+
+let sumOdd =[5,2,0,3,6,7];
+
+function sumOfOdd(n){
+  let isOdd = sumOdd[n]%2 != 0;
+  if(n==0){
+    return isOdd?sumOdd[n]:0;
+  }
+  return (isOdd?sumOdd[n]:0) + sumOfOdd(n-1);
+}
+
+console.log(sumOfOdd(sumOdd.length-1));

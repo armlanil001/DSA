9def3ff183a415b531344786e8f2dc0c1a4cb340
Aaromal Anil <armlanil001@gmail.com>
2026-05-21 20:06:59 +0530

    binary search

---
+++ binarySearch.js
@@ -0,0 +1,23 @@
+let arr = [-1 ,0 ,3,6,8,12,21];
+let target = 8;
+
+function binarySearch(arr,target){
+    let left = 0;
+    let right = arr.length-1;
+
+    while(right >=left){
+        let mid = Math.floor((left+right)/2);
+
+        if(arr[mid]==target){
+            return mid;
+        }else if(arr[mid]<target){
+            left =mid+1;
+        }else{
+            right =mid-1
+        }
+
+    }
+    return -1;
+}
+
+console.log(binarySearch(arr,target));

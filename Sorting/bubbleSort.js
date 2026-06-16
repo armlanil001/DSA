c99c0a0034b17d6184c147c8aaa2a82efd6c5451
Aaromal Anil <aromal@kaizenstars.com>
2026-06-16 21:47:47 +0530

    feat: implement bubble sort algorithm in bubbleSort.js

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,22 @@
+let array = [5, 4, 9, 15, 7, 1, 0]
+
+function bubbleSort(arr) {
+    let n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        let isSwapped = false;
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                isSwapped = true;
+            }
+        }
+      if(!isSwapped) break;
+     
+    }
+    return arr;
+}
+
+console.log(bubbleSort(array), 'result');
